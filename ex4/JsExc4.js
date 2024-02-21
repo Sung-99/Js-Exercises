5cc717e628ca7e8ab93ff540bf6b5624bdbf2a42
Sung-99 <vitorhugomota99@hotmail.com>
2024-02-21 17:35:26 -0300

    New exercise

---
+++ ex4/JsExc4.js
@@ -0,0 +1,11 @@
+//Simple exercise from https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
+//Calculate the area of three triangle types. I just did some add up set to
+//look more fittable on html
+
+
+let MyString = document.querySelector(".rotate_h1_content").childNodes[0].data;
+
+
+let output = document.querySelector(".output");
+
+output.textContent = MyString.charAt(1);

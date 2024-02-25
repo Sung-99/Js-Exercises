34c39262809771410e49acca5a6c8c0c2ab01878
Sung-99 <vitorhugomota99@hotmail.com>
2024-02-25 17:36:10 -0300

    Almost finished

--- ex4/js4exe.js
+++ ex4/js4exe.js
@@ -16,19 +16,55 @@
 
 let Divouter = document.createElement("div");
 Divouter.className = "Outer-Quest";
-Divouter.style.width = "100%";
+
 Divouter.style.color = 'white';
+Divouter.style.paddingTop = '50px';
+Divouter.style.marginTop = '50px';
+Divouter.style.paddingLeft = '50px';
+Divouter.style.marginLeft = '50px';
+Divouter.style.paddingRight = '0px';
+Divouter.style.marginRight= '0px';
 
-let DivAnswer = document.createElement("div");
-DivAnswer.className = "Answer";
-DivAnswer.style.width = "100%";
-DivAnswer.style.color = 'white';
+let Answer = document.createElement("input");
+Answer.type = "number";
+Answer.style.width = "140px";
+Answer.placeholder = 'input a number';
 
 
-Divouter.appendChild(DivAnswer);
+let submittt = document.createElement("button");
+
+submittt.style.height = "20px";
+submittt.style.width = "70px";
+submittt.textContent = "Submit";
 
 
-let x = document.querySelector(".Answer");
+
+document.body.appendChild(Divouter);
+
+Divouter.appendChild(Answer);
+Divouter.appendChild(submittt);
 
 
-Divouter.textContent = "aaaaaaaaaa";
+
+
+
+
+
+submittt.addEventListener("click", (e) =>{
+    e.preventDefault();
+    
+    let randd = Math.floor((Math.random() * (10 - 0) + 0));
+    let giveAnswer = document.createElement('p');
+    giveAnswer.className = 'pElement';
+    Divouter.appendChild(giveAnswer);
+    (Answer.value == randd ) ? giveAnswer.textContent = 'You are right': giveAnswer.textContent = 'GOT WRONG' + ' the right number is ' + randd ;
+   /*
+    if(Divouter.style.height > '300px')
+    {
+        x = document.querySelectorAll('.pElement');
+        giveAnswer.textContent = x[0].textContent = ' my ass'; 
+    }
+*/
+});
+
+

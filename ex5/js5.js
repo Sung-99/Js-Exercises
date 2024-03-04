39181d95df3055dabcc3cf112cdd358448ac7368
Sung-99 <vitorhugomota99@hotmail.com>
2024-03-04 18:02:26 -0300

    start

---
+++ ex5/js5.js
@@ -0,0 +1,15 @@
+let dat = new Date('2024', '12');
+
+let newElementt = document.createElement('div');
+
+//let FirsInnerElement = document.createElement('div');
+
+
+
+document.body.appendChild(newElementt);
+
+newElementt.style.width = '100px';
+newElementt.style.height = '100px';
+newElementt.style.background.color = 'black';
+
+newElementt.textContent = 'asdadsa';

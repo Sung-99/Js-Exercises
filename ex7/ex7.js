8ef8d41a53472a90c2f2415eece73119d85a9066
Sung-99 <vitorhugomota99@hotmail.com>
2024-03-19 22:32:03 -0300

    up

--- ex7/ex7.js
+++ ex7/ex7.js
@@ -1,1 +1,27 @@
 
+let options = document.querySelector('#optionsList');
+let convr = document.querySelector('#Convert')
+
+let out = document.querySelector('#answer_converted');
+
+
+
+convr.addEventListener("click", () =>{
+
+    let collection = options.selectedOptions;
+    let output = "";
+    for (let i = 0; i < collection.length; i++) {
+      
+        output += collection[i].label;
+  
+        if (i === collection.length - 2 && collection.length < 3) {
+          output += " and ";
+        } else if (i < collection.length - 2) {
+          output += ", ";
+        } else if (i === collection.length - 2) {
+          output += ", and ";
+        }
+      }
+      out.value = output;
+
+})

5a597cc9d6b3df3a854d96df31abc316b4531fcc
Sung-99 <vitorhugomota99@hotmail.com>
2024-03-24 18:44:22 -0300

    up

--- ex7/ex7.js
+++ ex7/ex7.js
@@ -10,15 +10,18 @@ let inputEntry = document.querySelector('#answer');
 
 let myBin = [];
 
+
+
 convr.addEventListener("click", () =>{
 
-
+      
       let convertedNumb = parseInt(inputEntry.value);  
       if(options.selectedOptions[0].label == "Decimal" && options2.selectedOptions[0].label == "Binario" )
       {
-        //Easy solution*****
+        
        while(convertedNumb >= 1){ 
         if(convertedNumb % 2 == 0) {
+            //Easy solution***** using push and toReversed()//join() js functions
             myBin.push(0);
            
         }else{
@@ -31,7 +34,38 @@ convr.addEventListener("click", () =>{
        
        let instantialVariablw = myBin.toReversed().join("");
        out.value = instantialVariablw;
-       
+       instantialVariablw = 0;
+      }
+      
+     
+      
+      if(options.selectedOptions[0].label == "Binario" && options2.selectedOptions[0].label == "Decimal" )
+      {
+       let onlyBin = 0;
+       let result = 0; 
+       let castNumb =inputEntry.value;
+       let lengthMyBinNumber = castNumb.length ;
+       let xInitial = 0;
+       while(xInitial < castNumb.length){
+        
+        if(castNumb[xInitial] != "1" && castNumb[xInitial] != "0"){
+            onlyBin++;
+        }
+        
+            xInitial++;
+       }
+       xInitial = 0;
+       while(xInitial < castNumb.length){
+        lengthMyBinNumber--;
+        if(castNumb[xInitial] == "1")result = result + (parseInt(castNumb[xInitial]) * (Math.pow(2, lengthMyBinNumber)))
+            xInitial++;
+       }
+       if(onlyBin > 0){
+        out.value ='Apenas numeros 1s e 0s';
+       }else{
+        out.value = result;
+       }
+       onlyBin = 0;
        
       } 
       

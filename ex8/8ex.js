59fb83e32fa3da3a22fcc15c46e09b2e1a935915
Sung-99 <vitorhugomota99@hotmail.com>
2024-06-24 14:31:52 -0300

    up ex8

--- ex8/8ex.js
+++ ex8/8ex.js
@@ -1,4 +1,14 @@
+let Calc = document.querySelector("button");
 
- 
-  
-    
+
+let answer = document.querySelector("#Answer");
+
+
+let date = new Date();
+let christmasDate = new Date(2024, 11, 25);
+
+
+ Calc.onclick = function(){
+  answer.textContent = Math.round((christmasDate.getTime()/(1000*3600*24))-(Date.now()/(1000*3600*24)));
+}
+

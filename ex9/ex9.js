965f950501be9e405fb8b78214509cf715f8397a
Sung-99 <vitorhugomota99@hotmail.com>
2024-06-30 10:12:27 -0300

    up ex9

--- ex9/ex9.js
+++ ex9/ex9.js
@@ -1,1 +1,13 @@
+let button = document.querySelector('#btn-make');
+let numberInputed = document.querySelector("#in-number");
+let answerArea = document.querySelector('#answer-solution');
 
+
+
+button.addEventListener("click", () =>{
+
+    answerArea.textContent = numberInputed.value;
+
+
+});
+    

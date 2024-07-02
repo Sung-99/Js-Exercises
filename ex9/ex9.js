2eb9688f62cb6ebfcba2fc65a308d73f43513c66
Sung-99 <vitorhugomota99@hotmail.com>
2024-07-01 23:17:33 -0300

    up

--- ex9/ex9.js
+++ ex9/ex9.js
@@ -8,7 +8,7 @@ let answer;
 button.addEventListener("click", () =>{
     answer = numberInputed.value-13;
 
-     ( numberInputed.value > 13) ? answerArea.textContent = 2*Math.abs(numberInputed.value-13): answerArea.textContent = numberInputed.value-13;
+     ( numberInputed.value > 13) ? answerArea.textContent = 2*Math.abs(numberInputed.value-13): answerArea.textContent = 13-numberInputed.value;
     //answerArea.textContent = numberInputed.value; // this work
    // answerArea.textContent = inputValue // *this show nothing
 });

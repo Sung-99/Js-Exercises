2caf2bc59ccb110bc6a517d0c7777e6d11df0625
Sung-99 <vitorhugomota99@hotmail.com>
2024-07-01 23:09:55 -0300

    up

--- ex9/ex9.js
+++ ex9/ex9.js
@@ -2,11 +2,14 @@ let button = document.querySelector('#btn-make');
 let numberInputed = document.querySelector("#in-number");
 let answerArea = document.querySelector('#answer-solution');
 
+let inputValue  = numberInputed.value;
 
-
+let answer;
 button.addEventListener("click", () =>{
+    answer = numberInputed.value-13;
 
-    answerArea.textContent = numberInputed.value;
-
+     ( numberInputed.value > 13) ? answerArea.textContent = 2*Math.abs(numberInputed.value-13): answerArea.textContent = numberInputed.value-13;
+    //answerArea.textContent = numberInputed.value; // this work
+   // answerArea.textContent = inputValue // *this show nothing
 });
     

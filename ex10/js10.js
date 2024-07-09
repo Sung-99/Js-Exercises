6bac7f253bda2e2d4a350042e186cd24195d9396
Sung-99 <vitorhugomota99@hotmail.com>
2024-07-09 16:42:50 -0300

    up

--- ex10/js10.js
+++ ex10/js10.js
@@ -3,7 +3,4 @@
 const matches = (obj, source) =>
     // Iterate over each key in the source object and check if it exists in the obj object and has the same value.
     Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] === source[key]);
-  // Test cases:
-  console.log(matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
-  console.log(matches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
-  console.log(matches({ hair: 'long', beard: true }, { age: 26, hair: 'long', beard: true })); // false 
+  

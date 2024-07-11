1cd736a5951b4412d6237402e33a50482e105ee5
Sung-99 <vitorhugomota99@hotmail.com>
2024-07-11 12:51:26 -0300

    up

--- ex10/js10.js
+++ ex10/js10.js
@@ -1,20 +1,53 @@
 
-let objs_Name1 = document.querySelector("#inp-name1");
-let objs_Name2 = document.querySelector("#inp-name2");
 
 
-let aa = document.querySelector("#aa");
-
-//#Source https://bit.ly/2neWfJ2
-// Define a function called `matches` that takes two objects as arguments and checks if the first object contains all the key-value pairs of the second object.
-const matches = (obj, source) =>
-    // Iterate over each key in the source object and check if it exists in the obj object and has the same value.
-    Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] === source[key]);
-  
-
+let inp_Name1 = document.querySelector("#inp-name1");
+let inp_Name2 = document.querySelector("#inp-name2");
 let compare =  document.querySelector("#compare");
+let objec1 = {};
+let objec2 = {};
+
+
+function deepEqual(object1, object2) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+  
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+  
+    for (const key of keys1) {
+      const val1 = object1[key];
+      const val2 = object2[key];
+      const areObjects = isObject(val1) && isObject(val2);
+      if (
+        areObjects && !deepEqual(val1, val2) ||
+        !areObjects && val1 !== val2
+      ) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  function isObject(object) {
+    return object != null && typeof object === 'object';
+  }
+
 
 compare.onclick = function (){
 
-    aa.textContent = objs.value;
+    objec1 =
+    { 
+        name: inp_Name1.value,
+        
+    };
+    objec2 =
+    { 
+        name: inp_Name2.value,
+        
+    };
+    console.log(deepEqual(objec1, objec2));
 }  
+

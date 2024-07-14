fc589d5044d1586720a4f8df123554b6682c53ea
Sung-99 <vitorhugomota99@hotmail.com>
2024-07-14 14:03:17 -0300

    up

--- ex11/js11.js
+++ ex11/js11.js
@@ -1,4 +1,19 @@
 //#Source https://bit.ly/2neWfJ2
+let btn_var = document.querySelector('button');
+let created_element = document.querySelector('.created-element');
+let textarea = document.querySelector('textarea');
+
+btn_var = document.addEventListener("click",triggeredbtn =>{
+
+   // const el = document.createElement('p');
+    //created_element.appendChild(el);
+    //el.textContent = textarea.value;
+    const selected =
+    document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+    textarea.select();
+    document.execCommand('copy');
+
+});
 const copy_to_Clipboard = str => {
     const el = document.querySelector('textarea');
     

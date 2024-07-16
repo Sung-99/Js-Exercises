e02c53742a0cfb112ecbbdcd66c13236570ae5a1
Sung-99 <vitorhugomota99@hotmail.com>
2024-07-16 14:32:41 -0300

    up

--- ex11/js11.js
+++ ex11/js11.js
@@ -3,17 +3,18 @@ let btn_var = document.querySelector('button');
 let created_element = document.querySelector('.created-element');
 let textarea = document.querySelector('textarea');
 
-btn_var = document.addEventListener("click",triggeredbtn =>{
-
-
+    
+btn_var = document.addEventListener('click', triggered => {
+    
+    
     
     const selected =
     document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-  if (selected) {
-    document.getSelection().removeAllRanges();
-    document.getSelection().addRange(selected);
-  }
+    textarea.select()
+    console.log(selected);
+    if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
+    }//Not filled with data
+
 });

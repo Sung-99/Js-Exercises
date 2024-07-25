031fadf08437e4a3710a75af124a761659f4e471
Sung-99 <vitorhugomota99@hotmail.com>
2024-07-25 14:50:20 -0300

    up

--- ex13/js13.js
+++ ex13/js13.js
@@ -1,19 +1,12 @@
-let stringg = document.querySelector('input');
+//let stringg = document.querySelector('input');
+
+
 let btn = document.querySelector('button');
+let p = document.querySelector('.example-text');
+
+let x = p.innerText.toString();
+let y = x.split('\n')
 
 btn.onclick = function(){
-    let p = '', row = [''], ret = [row], i = 0, r = 0, s = !0, l;
-    for (l of stringg.value) {
-        if ('"' === l) {
-            if (s && l === p) row[i] += l;
-            s = !s;
-        } else if (',' === l && s) l = row[++i] = '';
-        else if ('\n' === l && s) {
-            if ('\r' === p) row[i] = row[i].slice(0, -1);
-            row = ret[++r] = [l = '']; i = 0;
-        } else row[i] += l;
-        p = l;
-    }
-    console.log(ret);
-
+    console.log(y);
 };

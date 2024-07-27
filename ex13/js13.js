ce0500915749b39a23a3432543be8a9da950eed6
Sung-99 <vitorhugomota99@hotmail.com>
2024-07-27 14:34:40 -0300

    up

--- ex13/js13.js
+++ ex13/js13.js
@@ -1,4 +1,4 @@
-//let stringg = document.querySelector('input');
+let stringg = document.querySelector('input');
 
 
 const p = document.querySelector('.example-text');
@@ -6,7 +6,7 @@ const p = document.querySelector('.example-text');
 // This is the modern way to set up event handlers:
 document.querySelector('button').addEventListener("click", function(){
   // First, clean up leading or trailing spaces from the whole string
-  let x = p.textContent.trim();  
+  let x =stringg.value.trim();  
   
   // Split the string at newlines into an array,  then loop over that array and 
   // trim each item within the array and return that item into the new array

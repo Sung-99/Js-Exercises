0b890b769a2f4e2d1c694f628974cfd5f0f374f4
Sung-99 <vitorhugomota99@hotmail.com>
2024-07-27 14:11:37 -0300

    up

--- ex13/js13.js
+++ ex13/js13.js
@@ -1,12 +1,18 @@
 //let stringg = document.querySelector('input');
 
 
-let btn = document.querySelector('button');
-let p = document.querySelector('.example-text');
+const p = document.querySelector('.example-text');
 
-let x = p.innerText.toString();
-let y = x.split('\n')
-
-btn.onclick = function(){
-    console.log(y);
-};
+// This is the modern way to set up event handlers:
+document.querySelector('button').addEventListener("click", function(){
+  // First, clean up leading or trailing spaces from the whole string
+  let x = p.textContent.trim();  
+  
+  // Split the string at newlines into an array,  then loop over that array and 
+  // trim each item within the array and return that item into the new array
+  // returned by the map() function.
+  let y = x.split('\n').map(function(item){
+    return item.trim();
+  });
+  console.log(y);
+});

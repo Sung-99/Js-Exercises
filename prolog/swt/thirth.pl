960fb6e102b1f16928c88a373202624426cbe877
Sung-99 <vitorhugomota99@hotmail.com>
2024-09-15 09:48:21 -0300

    up

--- prolog/swt/thirth.pl
+++ prolog/swt/thirth.pl
@@ -2,4 +2,5 @@ concatenar([], L, L).
 concatenar([X|L1], L2, [X|L3]) :- concatenar(L1, L2, L3).
 
 reverter([], []).
-reverter(L, [X|Rest]):- reverter(L, Rest).
+reverter([X|Rest], Reversed):- reverter(Rest, ReversedTail), 
+concatenar(ReversedTail, [X], Reversed).

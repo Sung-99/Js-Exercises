c277e7078cf2c6b07781cd13fa403706fd61b84c
Sung <vitorhugomota99@hotmail.com>
2025-05-08 09:33:08 -0300

    up

--- ex20/ex20.js
+++ ex20/ex20.js
@@ -11,14 +11,64 @@ rl.on('line', (input) => {
     if (linhas.length < 1) {
         linhas.push(Number((input.split(' '))[0]));
         linhas.push(Number((input.split(' '))[1]));
-        linhas.push(Number((input.split(' '))[2]));    
-        Sort(linhas);
+        linhas.push(Number((input.split(' '))[2]));
+        quickSort(linhas, 0, linhas.length); 
         rl.close();
     }
+    for (let i = 0; i < linhas.length; i++) {
+        console.log(linhas[i] + " ");
+}   
 });
 
+function partition(arr, low, high)
+{
 
-function Sort(S){
-	 console.log(typeof Number(linhas[1]) === 'number');
+    // Choose the pivot
+    let pivot = arr[high];
 
+    // Index of smaller element and indicates
+    // the right position of pivot found so far
+    let i = low - 1;
+
+    // Traverse arr[low..high] and move all smaller
+    // elements to the left side. Elements from low to
+    // i are smaller after every iteration
+    for (let j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+
+    // Move pivot after smaller elements and
+    // return its position
+    swap(arr, i + 1, high);
+    return i + 1;
 }
+
+// Swap function
+function swap(arr, i, j)
+{
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+// The QuickSort function implementation
+function quickSort(arr, low, high)
+{
+    if (low < high) {
+
+        // pi is the partition return index of pivot
+        let pi = partition(arr, low, high);
+
+        // Recursion calls for smaller elements
+        // and greater or equals elements
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+
+
+

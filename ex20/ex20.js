1977446d0b4f967661f0c59db89a8b178ce4859f
Sung <vitorhugomota99@hotmail.com>
2025-05-10 01:27:56 -0300

    up

--- ex20/ex20.js
+++ ex20/ex20.js
@@ -17,7 +17,7 @@ rl.on('line', (input) => {
     }
     for (let i = 0; i < linhas.length; i++) {
         console.log(linhas[i] + " ");
-}   
+    }   
 });
 
 function partition(arr, low, high)
@@ -70,5 +70,29 @@ function quickSort(arr, low, high)
 }
 
 
+//
 
+// Optimized javaScript implementation
+// of Bubble sort
+/*function bubbleSort(arr, n){
+    var i, j, temp;
+    var swapped;
+    for (i = 0; i < n - 1; i++){
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++){
+            if (arr[j] > arr[j + 1]) 
+            {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
 
+        // IF no two elements were 
+        // swapped by inner loop, then break
+        if (swapped == false)
+        break;
+    }
+}*/

06b85ab01a4556560658a391d0aa30e58d4ae9a2
Sung <vitorhugomota99@hotmail.com>
2025-05-23 13:06:37 -0300

    úp

---
+++ ex21/ex21.ts
@@ -0,0 +1,12 @@
+
+
+
+const Test = require('@codewars/test-compat');
+
+describe("maskify", function(){
+  it("should work for some examples", function(){
+    Test.assertEquals(maskify('4556364607935616'), '############5616');
+    Test.assertEquals(maskify('1'), '1');
+    Test.assertEquals(maskify('11111'), '#1111');
+  });
+});
